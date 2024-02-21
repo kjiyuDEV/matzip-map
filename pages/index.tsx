23db0d8732c7ec033776c470af9cfaf5c6303aeb
kjiyuDEV <kjiyu1379@gmail.com>
2024-02-22 00:30:50 +0900

    fetch 누락

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,11 @@ import Image from "next/image";
 import Header from "@/components/common/Header";
 import { Fragment, useState } from "react";
 import MapComponent from "@/components/map/MapComponent";
-// import dbConnect from "@/db/database";
 ;
 
 
 export default function Home() {
   const [searchInput, setSearchInput] = useState<string>('');
-  // const client = await dbConnect;
-  // console.log(client)
 
   return (
     <Fragment>
